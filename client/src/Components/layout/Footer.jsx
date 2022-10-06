672d363a5e228441c21434bbaf99c4ed7febe272
ridaoc19 <ridaoc19@gmail.com>
2022-10-06 14:03:13 +0200

    css footer

--- client/src/Components/layout/Footer.jsx
+++ client/src/Components/layout/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './footer.module.css'
 
 function Footer(props) {
   return (
     <div>
-      <h1>pies de pagina</h1>
+      <div className={styles.container} >
+        <div className={styles.email} >
+          <h1>pies de pagina</h1>
+        </div>
+      </div>
     </div>
   );
 }
